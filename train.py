e8e38f029979ab5b19c72bb7a38b5dac63406b62
dmosc <osdavrm@gmail.com>
2025-11-01 08:55:49 -0700

    Use FlashAttention algorithm.
    FlashAttention is a rewrite of the original attention algorithm that is much more memory efficient since it does less roundtrips to the HBM.
    
    https://arxiv.org/pdf/2205.14135

--- train.py
+++ train.py
@@ -81,12 +81,19 @@ class CausalSelfAttention(nn.Module):
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         # attention (materializes the large (T, T) matrix for all the queries
         # and keys).
-        att = (q @ k.transpose(-2, -1) *
-               (1.0 / torch.sqrt(torch.tensor(k.size(-1)))))
-        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
-        att = F.softmax(att, dim=-1)
-        # (B, nh, T, T) @ (B, nh, T, hs) -> (B, nh, T, hs)
-        y = att @ v
+
+        # Original way to compute attention.
+        # att = (q @ k.transpose(-2, -1) *
+        #        (1.0 / torch.sqrt(torch.tensor(k.size(-1)))))
+        # att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        # att = F.softmax(att, dim=-1)
+        # # (B, nh, T, T) @ (B, nh, T, hs) -> (B, nh, T, hs)
+        # y = att @ v
+
+        # FlashAttention is a more memory-efficient way to compute attention.
+        # https://arxiv.org/pdf/2205.14135
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+
         # re-assemble all head outputs side by side.
         y = y.transpose(1, 2).contiguous().view(B, T, C)
         # output projection
