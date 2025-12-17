4f79a5241d219f666cae9a72f53fa9309255b10a
dmosc <osdavrm@gmail.com>
2025-12-16 22:01:40 -0800

    Simplify cross entropy.
    My homemade version doesn't seem to work all that well so I'm resorting to the legit implementation to unblock me and will revisit later.

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -2,13 +2,7 @@ import torch
 
 
 def cross_entropy(logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-    logits -= logits.max(dim=-1, keepdim=True).values
-    exp_logits = logits.exp()
-    sum_exp = exp_logits.sum(dim=-1)
-    target_exp = exp_logits.gather(dim=-1,
-                                   index=targets.unsqueeze(-1)).squeeze(-1)
-    loss = -(sum_exp / target_exp).log()
-    return loss.mean()
+    return torch.nn.functional.cross_entropy(logits, targets)
 
 
 def perplexity(mean_cross_entropy: torch.Tensor) -> torch.Tensor:
