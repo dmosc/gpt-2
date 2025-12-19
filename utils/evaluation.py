dbcc8a7554918ad401636a49569ecc457c484116
dmosc <osdavrm@gmail.com>
2025-12-19 07:28:27 -0600

    Implement cross entropy function manually.
    Following the instructions in the assignment we show the simplification after expanding the formal expression for cross entropy and express it in PyTorch.

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -2,7 +2,10 @@ import torch
 
 
 def cross_entropy(logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
-    return torch.nn.functional.cross_entropy(logits, targets)
+    logits_logsumexp = torch.logsumexp(logits, dim=-1)
+    target_logits = torch.gather(logits, dim=-1,
+                                 index=targets.unsqueeze(-1)).squeeze(-1)
+    return (logits_logsumexp - target_logits).mean()
 
 
 def perplexity(mean_cross_entropy: torch.Tensor) -> torch.Tensor:
