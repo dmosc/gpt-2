92e8022ebc54f9f781474e7bf9305f918d0d47c5
dmosc <osdavrm@gmail.com>
2025-12-24 16:06:17 -0600

    Add argument parser to modify from CLI.
    Using argparse we can directly extract CLI flags provided by the user.

--- app/train_all.py
+++ app/train_all.py
@@ -1,3 +1,5 @@
+import argparse
+
 from pathlib import Path
 from wakepy import keep
 from trainer import Trainer
@@ -5,9 +7,17 @@ from model.modules.config import Config
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='train_all.py',
+                                     description='Train tokenizer and model sequentially.',
+                                     epilog='Example usage: python app/train_all.py --skip_tokenizer_training')
+    parser.add_argument('--skip_tokenizer_training', action='store_true')
+    args = parser.parse_args()
     with keep.presenting():
         data_dir = Path(__file__).parent / 'data'
         config = Config(data_dir)
         trainer = Trainer(config)
-        trainer.train_tokenizer()
+        if args.skip_tokenizer_training:
+            print("Skipping tokenizer training. Loading from memory.")
+        else:
+            trainer.train_tokenizer()
         trainer.train_model()
