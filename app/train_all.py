6b8b7fab6cca15dd404cdaf498524156f31a0b41
dmosc <osdavrm@gmail.com>
2025-12-24 11:12:13 -0600

    Refactor trainer scripts into a Trainer class.
    This class manages the full training process both for the tokenizer as well as the model itself. I'll continue to refactor the Trainer to keep it more succinct in follow up CLs.

--- app/train_all.py
+++ app/train_all.py
@@ -1,9 +1,11 @@
 from wakepy import keep
-from train_tokenizer import train_tokenizer
-from train_model import train_model
+from trainer import Trainer
+from config import Config
 
 
 if __name__ == '__main__':
     with keep.presenting():
-        train_tokenizer()
-        train_model()
+        config = Config()
+        trainer = Trainer(config)
+        trainer.train_tokenizer()
+        trainer.train_model()
---
+++ app/trainer.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+
+from model.modules import LanguageModel
+from model.modules.optimizers import AdamW
+from model.modules.schedulers import CosAnnealingScheduler
+from model.modules.utils import Evaluator, grad_clip, DataLoader, Checkpointer, Tokenizer
+from config import Config
+
+
+class Trainer:
+    def __init__(self, config: Config):
+        self.config = config
+        self.base_dir = Path(__file__).resolve().parent
+
+    def train_model(self):
+        print('Training model...')
+        model = LanguageModel(self.config.d_model, self.config.num_heads,
+                              self.config.d_ff, self.config.vocab_size,
+                              self.config.max_seq_len, self.config.num_layers)
+        scheduler = CosAnnealingScheduler(self.config.max_lr,
+                                          self.config.min_lr,
+                                          self.config.warmup_steps,
+                                          self.config.max_steps)
+        optimizer = AdamW(list(model.parameters()), lr=scheduler.max_lr,
+                          weight_decay=self.config.weight_decay)
+        base_dir = self.base_dir
+        dataloader = DataLoader(self.config.tokenizer,
+                                base_dir / self.config.data_path,
+                                self.config.batch_size, self.config.seq_len)
+        checkpointer = Checkpointer(base_dir / self.config.checkpoint_dir)
+        evaluator = Evaluator()
+
+        for epoch in range(self.config.epochs):
+            step = 0
+            print(f'{epoch=}')
+            while data_paylaod := dataloader.get_next_batch():
+                batch, targets = data_paylaod
+                # Update learning rates following a cosine annealing schedule.
+                lr = scheduler.get_lr(step)
+                for param_group in optimizer.param_groups:
+                    param_group['lr'] = lr
+                # Compute gradients, loss and perform a step.
+                optimizer.zero_grad()
+                logits = model(batch)
+                # Reshape for cross entropy
+                # (batch_size * seq_len, vocab_size)
+                logits = logits.reshape(-1, logits.size(-1))
+                # (batch_size * seq_len,)
+                targets = targets.reshape(-1)
+                loss = evaluator.evaluate(step, logits, targets)
+                loss.backward()
+                grad_clip(model.parameters(), max_norm=0.1)
+                optimizer.step()
+                step += 1
+                if step % self.config.save_every_n_steps == 0:
+                    checkpointer.save_checkpoint(step, model, optimizer,
+                                                 evaluator, self.config)
+
+    def train_tokenizer(self):
+        tokenizer = Tokenizer()
+        tokenizer.train(self.base_dir / self.config.valid_data_path,
+                        self.config.max_vocab_size,
+                        [b'<|endoftext|>'])
