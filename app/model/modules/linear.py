4e9fc85943c33fa7157c91974c8c0b4062dbc236
dmosc <osdavrm@gmail.com>
2025-12-25 13:44:14 -0600

    Implement unit tests for Linear module.
    Starting with unittest + parameterized to start testing the entire project.

--- app/model/modules/linear.py
+++ app/model/modules/linear.py
@@ -5,12 +5,12 @@ from einops import einsum
 class Linear(torch.nn.Module):
     def __init__(self, in_features, out_features, device=None, dtype=None):
         super().__init__()
-        std = 2 / (in_features + out_features)
+        self.std = 2 / (in_features + out_features)
         weights = torch.rand(size=(out_features, in_features), dtype=dtype)
         weights = torch.nn.init.trunc_normal_(weights,
-                                              mean=0, std=std,
-                                              a=-3 * std ** 0.5,
-                                              b=3 * std ** 0.5)
+                                              mean=0, std=self.std,
+                                              a=-3 * self.std ** 0.5,
+                                              b=3 * self.std ** 0.5)
         self.weights = torch.nn.Parameter(weights)
         self.device = device
 
---
+++ tests/test_linear.py
@@ -0,0 +1,31 @@
+import torch
+import unittest
+
+from parameterized import parameterized
+
+from app.model.modules.linear import Linear
+
+
+class TestLinear(unittest.TestCase):
+    @parameterized.expand([
+        ((128, 384), (10, 128)),
+        ((128, 384), (10, 10, 128)),
+        ((128, 384), (10, 10, 10, 128)),
+    ])
+    def test_linear_forward(self, linear_dims: tuple, input_dims: tuple):
+        linear = Linear(linear_dims[0], linear_dims[1])
+        x = torch.rand(size=input_dims)
+        output = linear(x)
+
+        assert linear.weights.shape == (linear_dims[1], linear_dims[0])
+        assert type(linear.weights) == torch.nn.Parameter
+        assert linear.weights.requires_grad is True
+        self.assertAlmostEqual(linear.weights.std().item(), linear.std,
+                               delta=0.2)
+        self.assertTrue(torch.all(output >= -3 * linear.std ** 0.5))
+        self.assertTrue(torch.all(output <= 3 * linear.std ** 0.5))
+        assert output.shape == (*input_dims[:-1], linear_dims[1])
+
+
+if __name__ == '__main__':
+    unittest.main()
