ae306b1318f70ff1028e09151797806fb24a157c
dmosc <osdavrm@gmail.com>
2025-12-27 17:34:30 -0600

    Add tests for RMSNorm.

---
+++ tests/test_rmsnorm.py
@@ -0,0 +1,23 @@
+import torch
+import unittest
+
+from parameterized import parameterized
+
+from app.model.modules.rmsnorm import RMSNorm
+
+
+class TestRMSNorm(unittest.TestCase):
+    @parameterized.expand([
+        128
+    ])
+    def test_rmsnorm(self, d_model: int):
+        rmsnorm = RMSNorm(d_model)
+        x = torch.rand(size=(d_model,))
+        output = rmsnorm(x)
+
+        assert rmsnorm.gain.shape == (d_model,)
+        assert type(rmsnorm.gain) == torch.nn.Parameter
+        assert rmsnorm.gain.requires_grad is True
+        assert torch.all(rmsnorm.gain == 1)
+        assert output.shape == (d_model,)
+        assert torch.allclose(output, torch.ones_like(output), rtol=1)
